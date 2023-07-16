29bf366027d0c330eb8413180ee798a6e4bcc467
codiepoorzi1 <138014805+codiepoorzi1@users.noreply.github.com>
2023-07-16 17:00:50 +0000

    Pending changes exported from your codespace

---
+++ k.sh
@@ -0,0 +1,8 @@
+while [ 1 -ne 2  ]
+do
+	echo "salam"
+	sleep 1
+	echo "Khodoosh"
+	sleep 1
+	clear
+done
---
+++ kep.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+clear
+while [ 1 -ne 2 ]
+do
+    echo $RANDOM
+    sleep 1
+    echo $RANDOM
+    sleep 1
+    echo $RANDOM
+    sleep 1
+    echo $RANDOM
+    sleep 1
+    echo $RANDOM
+    sleep 1
+    echo $RANDOM
+    sleep 1
+    clear
+    sleep 1
+done
